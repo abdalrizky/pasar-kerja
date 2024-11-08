7f1b66ab3a7c9bfb1a50489d151e9776f8a88b14
Muhammad Abdal Rizky <rizky.muhammad425@gmail.com>
2024-11-08 18:46:50 +0800

    fixed style bugs

--- pages/index.php
+++ pages/index.php
@@ -11,6 +11,7 @@ $jobs = fetch("SELECT jobs.id, companies.logo AS 'company_logo', jobs.title, com
                 JOIN job_categories ON jobs.category_id = job_categories.id
                 LIMIT 4");
 
+
 ?>
 
 <!DOCTYPE html>
@@ -25,7 +26,18 @@ $jobs = fetch("SELECT jobs.id, companies.logo AS 'company_logo', jobs.title, com
 
 <body>
 
-    <?php include "../components/navbar.php" ?>
+    <header>
+        <a href="../pages/index.php">
+            <h1><span style="color: white;">Pasar</span><span style="color: orange; font-style: italic;">Kerja</span></h1>
+        </a>
+        <nav>
+            <ul>
+                <li><a href="index.php">Beranda</a></li>
+                <li><a href="job-seeker/bookmark.php">Bookmark</a></li>
+                <li><a href="job_seeker_dashboard.php">Hai, <?= $_SESSION['user']['name'] ?></a></li>
+            </ul>
+        </nav>
+    </header>
 
     <main>
         <section class="search-jobs">
